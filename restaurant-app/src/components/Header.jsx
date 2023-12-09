99f8283b873a0fb12871cabc2054b46311231808
lakshmiichoudhary <laxmi2252001@gmail.com>
2023-12-09 12:48:38 +0530

    created a header

---
+++ restaurant-app/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <>
+        <div className="nav-item">
+            <ul>
+                <ui>home</ui>
+                <ui>about</ui>
+                <ui>contact us</ui>
+                <ui>cart</ui>
+            </ul>
+
+        </div>
+        </>
+    )
+
+}
+
+export default Header
